62c34993adf1e156ebe732a3327b8bc11fbee144
Adam Coogan <acoogan@ucsc.edu>
2015-10-25 13:00:18 -0700

    Add functions for averaging over all files and plotting average counts

--- analyze.py
+++ analyze.py
@@ -72,9 +72,69 @@ def load_counts(path, first_check, move_step):
 
 def average_games():
     # Get counts for each game in the directory
-    counts_list_list = directory_map('KGS2005', lambda p: load_counts(p, 50, 50)[0])
+    first_sample = 20
+    move_step = 50
+    # TODO: change load_counts to take list of samples!!!
+    counts_list_list = directory_map('KGS2005', lambda p: load_counts(p, first_sample, move_step)[0])
 
-    max_binlen = max([len(counts) for counts in counts_list_list])
+    # Figure out largest group size for all checked moves in all games
+    max_num_groups = max([len(counts) for counts_list in counts_list_list for counts in counts_list])
+    max_num_samples = max([len(counts_list) for counts_list in counts_list_list])
+    num_games = len(counts_list_list)
+
+    # Create 3D array of all counts
+    all_counts = np.zeros([num_games, max_num_samples, max_num_groups])
+
+    for game_num, counts_list in enumerate(counts_list_list):
+        for sample_num, counts in enumerate(counts_list):
+            all_counts[game_num, sample_num, :counts.size] = counts
+
+    # Average group counts
+    average_over_games = np.mean(all_counts, 0)
+    error_over_games = np.std(all_counts, 0) / np.sqrt(num_games)
+
+    samples = range(first_sample, max_num_samples*move_step+first_sample, move_step)
+    plot_gamecube(average_over_games, error_over_games, samples)
+
+def plot_gamecube(average_over_games, error_over_games, samples):
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+
+    # Get highest group bin count
+    max_counts = max(np.ravel(average_over_games))
+    # Get largest group size
+    max_group_size = average_over_games.shape[1]
+    # Sexy colors
+    cmap = plt.get_cmap('jet')
+    # Width of the bars
+    num_samples = len(samples)
+    width = 1.0 / (num_samples + 1.0)
+
+    # Plot group size histogram
+    for i in range(num_samples):
+        counts = average_over_games[i, :]
+        # Can infer the correct bins by looking at the length of counts
+        group_sizes = np.arange(1, len(counts) + 1) + width * i - 0.5 + width / 2
+
+        ax.bar(group_sizes, counts, width, color = cmap(i / float(num_samples)), \
+                label = str(samples[i]) + ' moves')
+
+    # Make the plot pretty
+    ax.set_xlabel('Group sizes')
+    ax.set_ylabel('Counts')
+    ax.set_ylim(0.0, max_counts + 1)
+    ax.set_xlim(0.5, max_group_size + 1)
+    plt.legend(loc='upper right')
+
+    # Set ticks to appear between bins and to label x positions of bins
+    major_locator = MultipleLocator(1)
+    ax.xaxis.set_major_locator(major_locator)
+    plt.tick_params(which='major', length = 0)
+    minor_locator = FixedLocator(np.arange(0.5, max_group_size + 1.5, 1.0))
+    ax.xaxis.set_minor_locator(minor_locator)
+
+    # Draw the plot
+    plt.show()
 
 ##################################################
 
